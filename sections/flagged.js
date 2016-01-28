3b26027eb369c8f924c7d281cc611d03146499d0
davenich <davenich@gmail.com>
2016-01-28 16:55:28 -0500

    Take actions against flagged posts

--- sections/flagged.js
+++ sections/flagged.js
@@ -20,8 +20,55 @@
     });
   }])
   
-  .controller('flaggedController', ['$scope', function ($scope) {
+  .controller('flaggedController', [
+    '$scope', 
+    '$window', 
+    'firebaseService', 
+    '$firebaseArray', 
+  
+  function (
+    $scope, 
+    $window, 
+    firebaseService, 
+    $firebaseArray) {
     
+    function removeComment(id) {
+      var $ref = firebaseService.getRef('nodes', id);
+            
+      $ref.once('value', function ($snap) {
+        var removed = $snap.val();
+        
+        removed.name = 'removed';
+        removed.text = 'This comment was removed by a moderator.';
+        removed.removed = true;
+        
+        var $originRef = firebaseService.getRef(removed.origin);
+        
+        $ref.update(removed);
+        $originRef.update(removed, function () {
+          removeFlag(id);
+        });
+      });
+    }
+    
+    function removeFlag(id) {
+      var $ref = firebaseService.getRef('flagged', id);
+      $ref.remove();
+    }
+    
+    $scope.flaggedItems = $firebaseArray(firebaseService.getRef('flagged'));
+    
+    $scope.goTo = function (id) {
+      $window.open('https://www.birdyard.co/#/n/' + id);
+    };
+    
+    $scope.doAction = function (action, id) {
+      if (action === 'removeComment') {
+        removeComment(id);
+      } else if (action === 'ignore') {
+        removeFlag(id);
+      }
+    };
   }]);
   
 })(angular);
